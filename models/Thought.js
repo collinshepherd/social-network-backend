38713d0fd146e0eaf8090f57a180f8326fd4aeed
collinshepherd <77903396+collinshepherd@users.noreply.github.com>
2024-02-03 13:45:16 -0600

    Completed Models and User Controller

---
+++ models/Thought.js
@@ -0,0 +1,34 @@
+const { Schema, Types } = require("mongoose");
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      maxlength: 280,
+      minlength: 1,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: {
+      type: Schema.Types.ObjectId,
+      ref: "Reaction",
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+const Thought = model("thought", thoughtSchema);
+
+module.exports = Thought;
---
+++ models/User.js
@@ -0,0 +1,43 @@
+const { Schema, Types } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      validate: {
+        validator: function (v) {
+          return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v);
+        },
+        message: "Please enter a valid email",
+      },
+      required: [true, "Email required"],
+    },
+    thoughts: {
+      type: Schema.Types.ObjectId,
+      ref: "Thought",
+    },
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+const User = model("user", userSchema);
+
+module.exports = User;
